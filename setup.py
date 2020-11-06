74d472d91c86e52106dea7a8c8946b3ba9e09556
Brian Carlsen <carlsen.bri@gmail.com>
2020-11-06 13:13:38 +0100

    Updated package version.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ project_urls = {
 
 setuptools.setup(
     name = "easy-biologic",
-    version = "0.0.8",
+    version = "0.0.9",
     author = "Brian Carlsen",
     author_email = "carlsen.bri@gmail.com",
     description = "Controller class for communicating with BioLogic devices.",
